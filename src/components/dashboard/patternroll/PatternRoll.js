c50b7d6b9bd0c0b4193ee831424fa0c8310f10ed
sascha.bast <Qicklink1>
2019-01-12 08:53:00 +0100

    integratge gsui-lib

--- src/components/dashboard/patternroll/PatternRoll.js
+++ src/components/dashboard/patternroll/PatternRoll.js
@@ -40,37 +40,37 @@ export default class PatternRoll extends Component {
             <React.Fragment>
                 <Blockmanager/>
                 <Tracklist ref="tracklist"/>
-                    {this.props.children}
-                    <div id="gsuiPatternroll-template" className="gsuiBlocksManager gsuiPatternroll gsuiPanels-x" tabIndex="-1">
-                        <div className="gsuiBlocksManager-sidePanel">
-                            <div className="gsuiBlocksManager-sidePanelMenu">
-                                <a className="gsuiBlocksManager-magnet gsui-opacityHover">
-                                    <span className="gsuiBlocksManager-magnetValue"></span>
-                                </a>
-                            </div>
-                            <div className="gsuiBlocksManager-sidePanelContent"></div>
+                {this.props.children}
+                <div id="gsuiPatternroll-template" className="gsuiBlocksManager gsuiPatternroll gsuiPanels-x" tabIndex="-1">
+                    <div className="gsuiBlocksManager-sidePanel">
+                        <div className="gsuiBlocksManager-sidePanelMenu">
+                            <a className="gsuiBlocksManager-magnet gsui-opacityHover">
+                                <span className="gsuiBlocksManager-magnetValue"></span>
+                            </a>
                         </div>
-                        <div className="gsuiBlocksManager-gridPanel">
-                            <div className="gsuiBlocksManager-timelineWrap"></div>
-                            <div className="gsuiBlocksManager-rowsWrapout">
-                                <div className="gsuiBlocksManager-rows gsuiBeatlines">
-                                    <div className="gsuiBlocksManager-rowsWrapin">
-                                        <div className="gsuiBlocksManager-selection gsuiBlocksManager-selection-hidden"></div>
-                                        <div className="gsuiBlocksManager-currentTime"></div>
-                                        <div className="gsuiBlocksManager-loop gsuiBlocksManager-loopA"></div>
-                                        <div className="gsuiBlocksManager-loop gsuiBlocksManager-loopB"></div>
-                                    </div>
+                        <div className="gsuiBlocksManager-sidePanelContent"></div>
+                    </div>
+                    <div className="gsuiBlocksManager-gridPanel">
+                        <div className="gsuiBlocksManager-timelineWrap"></div>
+                        <div className="gsuiBlocksManager-rowsWrapout">
+                            <div className="gsuiBlocksManager-rows gsuiBeatlines">
+                                <div className="gsuiBlocksManager-rowsWrapin">
+                                    <div className="gsuiBlocksManager-selection gsuiBlocksManager-selection-hidden"></div>
+                                    <div className="gsuiBlocksManager-currentTime"></div>
+                                    <div className="gsuiBlocksManager-loop gsuiBlocksManager-loopA"></div>
+                                    <div className="gsuiBlocksManager-loop gsuiBlocksManager-loopB"></div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div id="gsuiPatternroll-block-template" className="gsuiBlocksManager-block gsuiPatternroll-block">
-                        <div className="gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropA"></div>
-                        <div className="gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropB"></div>
-                        <div className="gsuiPatternroll-block-header"></div>
-                        <div className="gsuiPatternroll-block-content"></div>
-                    </div>
+                <div id="gsuiPatternroll-block-template" className="gsuiBlocksManager-block gsuiPatternroll-block">
+                    <div className="gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropA"></div>
+                    <div className="gsuiBlocksManager-block-crop gsuiBlocksManager-block-cropB"></div>
+                    <div className="gsuiPatternroll-block-header"></div>
+                    <div className="gsuiPatternroll-block-content"></div>
+                </div>
 
             </React.Fragment>
 
